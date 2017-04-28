49c441d0344fa0f4d127dbbeeb4170c332ff4a8d
Kevin Ou <kevinou488@gmail.com>
2017-04-28 05:36:02 -0700

    try new commit return type

--- string.c
+++ string.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int my_strlen(char *s)
+size_t my_strlen(char *s)
 {
    char *p = s;
 
@@ -21,7 +21,7 @@ int main(void)
 
    for (i = 0; i < 2; ++i)
       
-   printf("string lenght of %s = %d\n", s[i], my_strlen(s[i]));
+   printf("string lenght of %s = %lu\n", s[i], my_strlen(s[i]));
 
    return 0;
 }
